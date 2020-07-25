ea152f42b1f0e7c89c20a2378eebce1c8a009aac
theriley106 <christopherlambert106@gmail.com>
2020-07-24 21:03:02 -0700

    Fixed code

--- isMask.py
+++ isMask.py
@@ -7,7 +7,7 @@ while True:
     small_frame = cv2.resize(video_capture.read()[1], (0, 0), fx=0.25, fy=0.25)
 
     if face_recognition.face_locations(small_frame, model="cnn"):
-        print({True: "FACE MASK ON", False: "FACE MASK OFF"}["top_lip" not in str(face_recognition.face_landmarks(small_frame))])
+    	print("FACE MASK ON" if "top_lip" not in str(face_recognition.face_landmarks(small_frame)) else "FACE MASK OFF")
     else:
     	print("NO FACE")
 
